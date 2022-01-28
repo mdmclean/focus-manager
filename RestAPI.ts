94a7ee9548d6658d0551eccac7360a7f4a414663
mdmclean <michaelmclean08@gmail.com>
2022-01-28 17:41:47 -0500

    create rest api for getting all next actions

--- RestAPI.ts
+++ RestAPI.ts
@@ -1,3 +1,16 @@
 function AddNextActionRow(name:string, description:string, priority:number, childOf:string, theme:string, points:number) {
     NextActionsDAL.AddRow(name,description,priority,childOf,theme,points);
 }
+
+function GetAllNextActions() {
+    let rows = NextActionsDAL.GetRows();
+
+    let rowsAsArrays = [];
+
+    rows.forEach((row) => {
+        rowsAsArrays.push( (Object.keys(row).map((key) => row[key])).join("||"));
+    })
+
+    return rowsAsArrays;
+}
+
