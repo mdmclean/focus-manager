2c2c67b75cc7fc49fa0036279d06af913b9bd40e
mdmclean <michaelmclean08@gmail.com>
2022-03-26 13:49:15 -0400

    add recurring action handling to the api initiator

--- RecurringActionsUpdater.ts
+++ RecurringActionsUpdater.ts
@@ -1,25 +1,24 @@
 import { DateAccessor } from "./DAL/DateAccessor";
 import { INextActionDataAccessor } from "./DAL/INextActionDataAccessor";
 import { IRecurringEventDataAccessor } from "./DAL/IRecurringEventDataAccessor";
-import { RecurringActionDAL } from "./DAL/RecurringEventDAL";
+import { RecurringActionDAL } from "./DAL/RecurringActionDAL";
 import { NextActionHelper } from "./Helpers/NextActionHelper";
 import { NextAction } from "./Models/Sheets/NextAction";
 import { RecurringAction } from "./Models/Sheets/RecurringAction";
 
 export module RecurringActionUpdater {
 
-  export function AddRecurringActions(naAccessor: INextActionDataAccessor) {
+  export async function AddRecurringActions(naAccessor: INextActionDataAccessor, raAccessor: IRecurringEventDataAccessor) {
 
-    let recurringActionAccessor:IRecurringEventDataAccessor = new RecurringActionDAL();
-    let recurringActions: RecurringAction[] = recurringActionAccessor.GetRows();
+    let recurringActions: RecurringAction[] = raAccessor.GetRows();
 
-    recurringActions.forEach((row) => {
+    await recurringActions.forEach(async (row) => {
       if (row.nextOccurrence < DateAccessor.Today()) {
 
         let newAction: NextAction = NextActionHelper.CreateActionWithDefaults(row.name, row.description, row.priority, row.childOf, row.targetTheme, row.points, 5, 3);
-        naAccessor.AddRow(newAction);
+        await naAccessor.AddRow(newAction);
         row.nextOccurrence = DateAccessor.GetDateXDaysFromNow(row.frequencyInDays);
-        recurringActionAccessor.Update(row);
+        await raAccessor.Update(row);
       }
     }
     )
